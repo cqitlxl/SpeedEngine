ba34b7e945603c4fcfc339d526e789920d7639fe
Diamond <cqitlxl@gmail.com>
2018-08-01 17:26:50 +0800

    创建测试工程

---
+++ app/src/main/java/com/diamond/speedengine/MainActivity.java
@@ -0,0 +1,22 @@
+package com.diamond.speedengine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * FileName:
+ * Des:
+ *
+ * @author lxl
+ * @author: diamond{diamond@gmail.com}
+ * @created: 2018-08-下午4:15
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
