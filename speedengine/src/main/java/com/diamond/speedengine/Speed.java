c719e311dd0859c70775f3298d73e2dac70fac33
Diamond <cqitlxl@gmail.com>
2018-08-01 21:14:14 +0800

    fixed untracked files

---
+++ speedengine/src/main/java/com/diamond/speedengine/Speed.java
@@ -0,0 +1,18 @@
+package com.diamond.speedengine;
+
+/**
+ * FileName:
+ * Des:
+ *
+ * @author diamond{diamond@gmail.com}
+ * @created 2018-08-01 下午8:51
+ */
+
+public class Speed {
+
+
+
+    public Speed(){
+    }
+
+}
